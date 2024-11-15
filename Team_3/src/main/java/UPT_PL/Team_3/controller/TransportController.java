ffa75943ed4e425104d3980edc6360ade8819e15
Fatimasania <Fatimasania@DESKTOP-BGMSE5K>
2024-11-15 23:51:08 +0000

    Created Classes : TransportController, TransportService and TransportRepository

---
+++ Team_3/src/main/java/UPT_PL/Team_3/controller/TransportController.java
@@ -0,0 +1,40 @@
+package UPT_PL.Team_3.controller;
+
+import UPT_PL.Team_3.model.Transport;
+import UPT_PL.Team_3.service.TransportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/transports")
+public class TransportController {
+
+    @Autowired
+    private TransportService transportService;
+
+    @GetMapping
+    public List<Transport> getAllTransports() {
+        return transportService.getAllTransports();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Transport> getTransportById(@PathVariable String id) {
+        Optional<Transport> transport = transportService.getTransportById(id);
+        return transport.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Transport createTransport(@RequestBody Transport transport) {
+        return transportService.createTransport(transport);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTransport(@PathVariable String id) {
+        transportService.deleteTransport(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ Team_3/src/main/java/UPT_PL/Team_3/service/TransportService.java
@@ -0,0 +1,40 @@
+package UPT_PL.Team_3.service;
+
+import UPT_PL.Team_3.model.Transport;
+import UPT_PL.Team_3.repository.TransportRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TransportService {
+
+    private final TransportRepository transportRepository;
+
+    @Autowired
+    public TransportService(TransportRepository transportRepository) {
+        this.transportRepository = transportRepository;
+    }
+
+    public List<Transport> getAllTransports() {
+        return transportRepository.findAll();
+    }
+
+    public Optional<Transport> getTransportById(String id) {
+        return transportRepository.findById(id);
+    }
+
+    public Transport createTransport(Transport transport) {
+        return transportRepository.save(transport);
+    }
+
+    public void deleteTransport(String id) {
+        if (transportRepository.existsById(id)) {
+            transportRepository.deleteById(id);
+        } else {
+            throw new RuntimeException("Transport not found with id: " + id);
+        }
+    }
+}
