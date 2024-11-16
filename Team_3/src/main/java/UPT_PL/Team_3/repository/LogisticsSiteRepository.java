346631cce6713f3efca2b2192277d8cfd171accd
Anastasiia <n21koketka@gmail.com>
2024-11-16 16:13:36 +0200

    Add LogisticsSiteRepository

---
+++ Team_3/src/main/java/UPT_PL/Team_3/repository/LogisticsSiteRepository.java
@@ -0,0 +1,17 @@
+package UPT_PL.Team_3.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import UPT_PL.Team_3.model.LogisticsSite;
+import UPT_PL.Team_3.model.RouteLine;
+
+@Repository
+public interface LogisticsSiteRepository extends JpaRepository<LogisticsSite, String> {
+	
+	@Query(value = "SELECT * FROM team_3.LogisticsSite where countryId =?1", nativeQuery = true)
+	List<RouteLine> getLogisticsSiteByCountry_Id(String countryId);
+}
