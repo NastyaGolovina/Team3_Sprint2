3397b36db7522b0e346f38bb2fe734e09bd496ef
Anastasiia <n21koketka@gmail.com>
2024-12-08 15:16:57 +0200

    Create and configure Team_3_GraphUI project

---
+++ Team_3_GraphUI/src/main/java/UPT_PL/Team_3_GraphUI/JavaFxLauncher.java
@@ -0,0 +1,10 @@
+package UPT_PL.Team_3_GraphUI;
+
+import javafx.application.Application;
+
+public class JavaFxLauncher {
+	
+	public static void main(String[] args) {
+//	       Application.launch(Name.class, args);	       
+	    }
+}
