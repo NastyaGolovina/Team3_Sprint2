2fd3f1b16fb3d1f0683793ab023a9810ba3acc0a
Lyly Nguyen <lyyngn1723@gmail.com>
2024-12-14 00:55:13 +0000

    Update addProduct and deleteProduct methods. Also create updateProduct method.

--- Team_3/src/main/java/UPT_PL/Team_3/model/Products.java
+++ Team_3/src/main/java/UPT_PL/Team_3/model/Products.java
@@ -142,69 +142,82 @@ public class Products {
 		    
 	
 	// ADJUST THE ADD PRODUCT METHOD so that a product can only be added if the all the conditions are met
-		//If any of these conditions fail, an appropriate error message is returned for each condition.
-		//If all conditions pass, the product is added, and an empty string is returned.
-		
-		//The method doesn’t create a new Product from scratch. 
-	    //Instead, it will just fill in the details (like productID, name,expirationInDays,recommendedRate) of the existing Product object here is newProduct .
+			//If any of these conditions fail, an appropriate error message is returned for each condition.
+			//If all conditions pass, the product is added, and an empty string is returned.
+			
+			//The method doesn’t create a new Product from scratch. 
+		    //Instead, it will just fill in the details (like productID, name,expirationInDays,recommendedRate) of the existing Product object here is newProduct .
 
-		public String addProduct(String productID, String name, Integer expirationInDays, double recommendedRate, // convert integer and double to string
-				Product newProduct) {
-			String output = "";
+			public String addProduct(String productID, String name, String expirationInDays, String recommendedRate, // convert integer and double to string
+					Product newProduct) {
+				String output = "";
 
-			if (productID != null && !productID.isBlank()) {
-				if (productID.matches("^[a-zA-Z0-9].*")) { // condition if productID begins with any special characters
-															// (i.e., non-alphanumeric characters),
-					if (productID.length() <= 20) {
-						int productPos = searchProduct(productID); // Product position in the array list
-						if (productPos == -1) { // if it is not found
+				if (productID != null && !productID.isBlank()) {
+					if (productID.matches("^[a-zA-Z0-9].*")) { // condition if productID begins with any special characters
+																// (i.e., non-alphanumeric characters),
+						if (productID.length() <= 20) {
+							int productPos = searchProduct(productID); // Product position in the array list
+							if (productPos == -1) { // if it is not found
 
-							// then validate the parameters
-							if (name != null && !name.isBlank()) {
-								if (expirationInDays != null) {
-									if (expirationInDays > 0) {
-										if (recommendedRate > 0) {
-											if (recommendedRate <= 10000) {
-												newProduct.setProductID(productID);
-												newProduct.setName(name);
-												newProduct.setExpirationInDays(expirationInDays);
-												ProductList.add(newProduct);
+								// then validate the parameters
+								  if (name != null && !name.isBlank()) {
+									if (expirationInDays != null && !expirationInDays.isBlank()) {
+										if (expirationInDays.matches("-?\\d+")) {
+											int expirationInDaysInt = Integer.parseInt(expirationInDays);
+											if (expirationInDaysInt > 0) {
+												if (recommendedRate != null && !recommendedRate.isBlank()) {
+													if (recommendedRate.matches("[-+]?\\d*\\.?\\d+([eE][-+]?\\d+)?")) {
+														double recommendedRateDouble = Double.parseDouble(recommendedRate);
+														if (recommendedRateDouble > 0) {
+
+															newProduct.setProductID(productID);
+															newProduct.setName(name);
+															newProduct.setExpirationInDays(expirationInDaysInt);
+															newProduct.setRecommendedRate(recommendedRateDouble);
+															ProductList.add(newProduct);
+
+														} else {
+															output = "The recommended rate must be greater than 0.";
+														}
+
+													} else {
+														output = "The recommended rate must double.";
+													}
+												} else {
+													output = "The recommended rate cannot be empty.";
+												}
 
 											} else {
-												output = "The recommended rate can not exceed 10000";
-											}
+												output = "The amount of days till expiration must be greater than 0)";
 
+											}
 										} else {
-											output = "The recommended rate must be greater than 0";
+											output = "The amount of days till expiration must be an integer";
+
 										}
 
 									} else {
-										output = "The amount of days till expiration date must be greater than 0";
+										output = "The amount of till expiration cannot be empty.";
 									}
-
 								} else {
-									output = "The amount of days till expiration date can not be empty";
+									output = "The name of the product cannot be empty.";
 								}
 
 							} else {
-								output = "The name of the product cannot be empty.";
+								output = "Product already exists with ID: " + productID;
 							}
 						} else {
-							output = "Product already exists with ID: " + productID;
+							output = "The ID cannot exceed more than 20 characters.";
 						}
-
 					} else {
-						output = "The ID cannot exceed more than 20 characters.";
+						output = "The ID of the product cannot begin with special characters.";
 					}
 				} else {
-					output = "The ID of the product cannot begin with special characters.";
+					output = "The ID of the product cannot be empty.";
 				}
 
-			} else {
-				output = "The ID of the product cannot be empty.";
-			}
-			return output;
-		}
+				return output;
+			}					
 		
 
 	/**
@@ -316,8 +329,8 @@ public String deleteProduct(String productID, ArrayList<Country> countries) {
 			}
 
 			RestAPIHelper restAPIHelper = new RestAPIHelper();
-			ResponseEntity<RouteLine[]> response = restAPIHelper.getRestTemplate().getForEntity(
-					restAPIHelper.getRootAPIURL() + "routeLines/product/" + productID, RouteLine[].class);
+			ResponseEntity<RouteLine[]> response = restAPIHelper.getRestTemplate()
+					.getForEntity(restAPIHelper.getRootAPIURL() + "routeLines/product/" + productID, RouteLine[].class);
 
 			if (response.getStatusCode().is2xxSuccessful()) {
 				RouteLine[] routeLineArr = response.getBody();
@@ -344,8 +357,64 @@ public String deleteProduct(String productID, ArrayList<Country> countries) {
 	return output;
 }
 
+// UPDATE PRODUCT
+
+public String updateProduct(String productID, String name, String expirationInDays, String recommendedRate,Product newProduct) {
+	String output = "";
+	if (productID != null && !productID.isBlank()) {
+		int productPos = searchProduct(productID); // Product position in the array list
+		if (productPos == -1) { // if it is not found
+			Product editedProduct = ProductList.get(productPos);
+			if (name != null && !name.isBlank()) {
+				if (expirationInDays != null && !expirationInDays.isBlank()) {
+					if (expirationInDays.matches("-?\\d+")) {
+						int expirationInDaysInt = Integer.parseInt(expirationInDays);
+						if (expirationInDaysInt > 0) {
+							if (recommendedRate != null && !recommendedRate.isBlank()) {
+								if (recommendedRate.matches("[-+]?\\d*\\.?\\d+([eE][-+]?\\d+)?")) {
+									double recommendedRateDouble = Double.parseDouble(recommendedRate);
+									if (recommendedRateDouble > 0) {
+										editedProduct.setName(name);
+										editedProduct.setExpirationInDays(expirationInDaysInt);
+										editedProduct.setRecommendedRate(recommendedRateDouble);
+										newProduct.setName(name);
+										newProduct.setExpirationInDays(expirationInDaysInt);
+										newProduct.setRecommendedRate(recommendedRateDouble);
+										
+									} else {
+										output = "The recommended rate must be greater than 0  ";
+									}
+							} else {
+								output = "The recommended rate must be double  ";
+							}
+						} else {
+							output = "The recommended rate cannot be empty.";
+						}
+					} else {
+						output = "The amount of till expiration must be greater than 0";
+					}
+					
+				} else {
+					output = "The amount of till expiration must be integer.";
+				}
+			}
+				else {
+					output = "The amount of till expiration cannot be empty.";
+				}
+			
+			
+		} else {
+			output = "Product's name can not be empty ";
+		}
+
+		
+	} else {
+		output = "Product already exists with ID: " + productID;
+	}
+
+
+}  else {
+	output = "The ID of the product cannot be empty.";
 }
-
-
-
-	
+	return output;
+}}
