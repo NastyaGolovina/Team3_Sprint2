7ed8cbca8a4720c9564a1c013dc782bb8f6095f3
Lyly Nguyen <lyyngn1723@gmail.com>
2024-12-17 21:20:09 +0000

    update ProductsByCountry controller and service

--- Team_3/src/main/java/UPT_PL/Team_3/controller/ProductsByCountryController.java
+++ Team_3/src/main/java/UPT_PL/Team_3/controller/ProductsByCountryController.java
@@ -1,7 +1,10 @@
 package UPT_PL.Team_3.controller;
 
-import UPT_PL.Team_3.model.ProductsByCountry;
 
+
+import UPT_PL.Team_3.model.Product;
+
+import UPT_PL.Team_3.model.ProductsByCountry;
 import UPT_PL.Team_3.service.ProductsByCountryService;
 
 import java.util.List;
@@ -9,47 +12,95 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
+
 import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.PathVariable;
+
 import org.springframework.web.bind.annotation.PostMapping;
+
+import org.springframework.web.bind.annotation.PutMapping;
+
 import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
+
 @RequestMapping("/api/products-by-country")
+
 public class ProductsByCountryController {
+
 		@Autowired
+
 		private ProductsByCountryService productsByCountryService;
 
+
+
 		@GetMapping
+
 		public List<ProductsByCountry> getAllProductsByCountry() {
+
 	        return productsByCountryService.getAllProductsByCountry();
+
 	    }
 
+
+
 		@GetMapping("/{id}")
+
 		public ResponseEntity<ProductsByCountry> getProductByCountryId(@PathVariable String  productByCountryId) {
+
 			Optional<ProductsByCountry> productsByCountry = productsByCountryService.getProductByCountryId( productByCountryId);
+
 			return productsByCountry.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+
 		}
 
+
+
 		@GetMapping("/country/{countryId}")
+
 		public List<ProductsByCountry> getProductsByCountryByCountryId(@PathVariable String countryId) {
+
 			return productsByCountryService.getProductsByCountryByCountryId(countryId);
+
 		}
+
 		
+
 		@PostMapping
+
 		public ProductsByCountry createProductsByCountry(@RequestBody ProductsByCountry productsByCountry) {
+
 			return productsByCountryService.createProductsByCountry(productsByCountry);
+
+		}
+
+		@PutMapping
+
+		public ProductsByCountry updateProductsByCountry(@RequestBody ProductsByCountry productsByCountry) {
+
+			return productsByCountryService.updateProductsByCountry(productsByCountry);
+
 		}
 
 		@DeleteMapping("/{productByCountryId}")
+
 		public ResponseEntity<Void> deleteProductsByCountry(@PathVariable String productByCountryId) {
+
 			productsByCountryService.deleteProductsByCountry(productByCountryId);
+
 			return ResponseEntity.noContent().build();
+
 		}
+
 	}
-
-
